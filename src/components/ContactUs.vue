422f0472fd8d87cea93afafdee4e3bda3b8c64a0
CJ Cucio <cuciocj@gmail.com>
2020-06-12 14:10:29 +1200

    updated component

--- src/components/ContactUs.vue
+++ src/components/ContactUs.vue
@@ -1,22 +1,60 @@
 <template>
-  <div>
-    <b-img
-      :src="require('@/assets/keyboard-type.jpg')"
-      alt="keyboard-type"
-      fluid-grow
-    >
-    </b-img>
+  <div class="contact-content">
+    <div class="img-overlay">
+      <div class="project-overlay">
+        <h1>GET IN TOUCH</h1>
+        <p class="text-center">
+          Once you are ready, you can make contact with Kerry.
+          You can call Kerry or email her with a preferred time to meet.
+          Meetings can be online or face to face - depending on where you live.
+        </p>
+      </div>
+      <b-img
+        id="bg-image"
+        :src="require('@/assets/computer-keyboard-3.jpg')"
+        alt="keyboard-type"
+        fluid-grow
+      >
+      </b-img>
+    </div>
   </div>
 </template>
 
 <style>
-h1 {
+.contact-content .project-overlay > h1 {
   text-align: center;
-  margin-top: 60px;
+  font-size: 5vw;
+  font-weight: bold;
+  font-family: "Lucida Sans", "Lucida Sans Regular", "Lucida Grande",
+    "Lucida Sans Unicode", Geneva, Verdana, sans-serif;
+  margin-top: 15vw;
 }
 
-span.text-overlay {
-  float: left;
+.contact-content .project-overlay > .text-center {
+  font-size: 1.5vw;
+  font-family: "Lucida Sans", "Lucida Sans Regular", "Lucida Grande",
+    "Lucida Sans Unicode", Geneva, Verdana, sans-serif;
+  margin: auto 15vw;
 }
 
+.img-overlay {
+  position: relative;
+}
+
+.project-overlay {
+  position: absolute;
+  background-color:#b0954580;
+  width: 100%;
+  height: 100%;
+  color: #fff;
+}
+
+.tinted-image {
+  background:
+    /* top, transparent red */
+    linear-gradient(
+      rgba(255, 0, 0, 0.45),
+      rgba(255, 0, 0, 0.45)
+    )
+}
 </style>

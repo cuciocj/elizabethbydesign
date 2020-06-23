9cdcecb25072cd40bbf75e47fd35ddf96a42c970
psantander <30874349+psantander@users.noreply.github.com>
2020-06-23 17:24:03 +1200

    Update Where.vue

--- src/components/Where.vue
+++ src/components/Where.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="where-content">
-    <h1>WHERE ARE YOU GOING?</h1>
+      <div>
+        <b-card no-body>
+          <b-tabs pills card vertical nav-wrapper-class="w-25">
+            <b-tab title="Tab 1" active><b-card-text>Tab contents 1</b-card-text></b-tab>
+            <b-tab title="Tab 2"><b-card-text>Tab contents 2</b-card-text></b-tab>
+            <b-tab title="Tab 3"><b-card-text>Tab contents 3</b-card-text></b-tab>
+          </b-tabs>
+        </b-card>
+      </div>
   </div>
 </template>
 

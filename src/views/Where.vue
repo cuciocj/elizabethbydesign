3596cc3d5377245f840a9e76ef18fc4a803e8446
psantander <30874349+psantander@users.noreply.github.com>
2020-06-26 16:11:31 +1200

    Update where
    changed nav bar to the top and scroll content

--- src/views/Where.vue
+++ src/views/Where.vue
@@ -1,156 +1,203 @@
 <template>
-  <b-container fluid>
-    <b-row>
-      <b-col id="my-list-col" cols="4">
-        <b-list-group id="my-list" v-b-scrollspy:listgroup-ex>
-          <b-list-group-item href="#list-item-1">Casual Dresses</b-list-group-item>
-          <b-list-group-item href="#list-item-2">Casual Tops</b-list-group-item>
-          <b-list-group-item href="#list-item-3">Corporate</b-list-group-item>
-          <b-list-group-item href="#list-item-4">Formal Dresses</b-list-group-item>
-          <b-list-group-item href="#list-item-5">Formal Coat</b-list-group-item>
-        </b-list-group>
-      </b-col>
+  <div class="where-content">
+    <b-card no-body>
 
-      <b-col cols="8">
-        <div id="listgroup-ex" class="example">
-          <h4 id="list-item-1">Casual Dresses</h4>
-          <b-row style="width: 1650px;">
-            <b-col v-for="n in 3" :key="n">
-              <b-card no-body style="width: 540px;">
-                <b-row no-gutters>
-                  <b-col md="6">
-                    <b-card-img src="../assets/casual_photos/casual2.jpg" class="rounded-0"></b-card-img>
-                  </b-col>
-                  <b-col md="6">
-                    <b-card-body title="Horizontal Card">
-                      <b-card-text>
-                        {{ text }}
-                      </b-card-text>
-                    </b-card-body>
-                  </b-col>
-                </b-row>
-              </b-card>
-            </b-col>
-          </b-row>
-          <br>
-          <h4 id="list-item-2">Casual Tops</h4>
-          <b-row style="width: 1650px;">
-            <b-col v-for="n in 3" :key="n">
-              <b-card no-body style="width: 540px;">
-                <b-row no-gutters>
-                  <b-col md="6">
-                    <b-card-img src="../assets/casual_photos/casual2.jpg" class="rounded-0"></b-card-img>
-                  </b-col>
-                  <b-col md="6">
-                    <b-card-body title="Horizontal Card">
-                      <b-card-text>
-                        {{ text }}
-                      </b-card-text>
-                    </b-card-body>
-                  </b-col>
-                </b-row>
-              </b-card>
-            </b-col>
-          </b-row>
-          <br>
-          <h4 id="list-item-3">Corporate</h4>
-          <b-row style="width: 1650px;">
-            <b-col v-for="n in 3" :key="n">
-              <b-card no-body style="width: 540px;">
-                <b-row no-gutters>
-                  <b-col md="6">
-                    <b-card-img src="../assets/casual_photos/casual2.jpg" class="rounded-0"></b-card-img>
-                  </b-col>
-                  <b-col md="6">
-                    <b-card-body title="Horizontal Card">
-                      <b-card-text>
-                        {{ text }}
-                      </b-card-text>
-                    </b-card-body>
-                  </b-col>
-                </b-row>
-              </b-card>
-            </b-col>
-          </b-row>
-          <br>
-          <h4 id="list-item-4">Formal Dresses</h4>
-          <b-row style="width: 1650px;">
-            <b-col v-for="n in 3" :key="n">
-              <b-card no-body style="width: 540px;">
-                <b-row no-gutters>
-                  <b-col md="6">
-                    <b-card-img src="../assets/casual_photos/casual2.jpg" class="rounded-0"></b-card-img>
-                  </b-col>
-                  <b-col md="6">
-                    <b-card-body title="Horizontal Card">
-                      <b-card-text>
-                        {{ text }}
-                      </b-card-text>
-                    </b-card-body>
-                  </b-col>
-                </b-row>
-              </b-card>
-            </b-col>
-          </b-row>
-          <br>
-          <h4 id="list-item-5">Formal Coat</h4>
-          <b-row style="width: 1650px;">
-            <b-col v-for="n in 3" :key="n">
-              <b-card no-body style="width: 540px;">
-                <b-row no-gutters>
-                  <b-col md="6">
-                    <b-card-img src="../assets/casual_photos/casual2.jpg" class="rounded-0"></b-card-img>
-                  </b-col>
-                  <b-col md="6">
-                    <b-card-body title="Horizontal Card">
-                      <b-card-text>
-                        {{ text }}
-                      </b-card-text>
-                    </b-card-body>
-                  </b-col>
-                </b-row>
-              </b-card>
-            </b-col>
-          </b-row>
-        </div>
-      </b-col>
-    </b-row>
-  </b-container>
+      <b-nav pills card-header align="center" slot="header" v-b-scrollspy:nav-scroller sticky>
+        <b-nav-item  href="#list-item-1" @click="scrollIntoView">Casual Dresses</b-nav-item>
+        <b-nav-item  href="#list-item-2" @click="scrollIntoView">Casual Shorts</b-nav-item>
+        <b-nav-item  href="#list-item-3" @click="scrollIntoView">Coorporate dresses</b-nav-item>
+        <b-nav-item  href="#list-item-4" @click="scrollIntoView">Formal Dresses</b-nav-item>
+        <b-nav-item  href="#list-item-5" @click="scrollIntoView">Formal Coat</b-nav-item>
+      </b-nav>
+
+      <b-card-body id="nav-scroller" class="my-content" ref="content" style="position:relative; height:500px;">
+        <b-col cols="12">
+          <div id="listgroup-ex">
+            <h4 id="list-item-1">Casual Dresses</h4>
+            <b-row style="width: 1650px;">
+              <b-col v-for="n in 3" :key="n">
+                <b-card no-body style="width: 540px;">
+                  <b-row no-gutters>
+                    <b-col md="6">
+                      <b-card-img src="../assets/casual_photos/casual2.jpg"
+                      class="rounded-0"></b-card-img>
+                    </b-col>
+                    <b-col md="6">
+                      <b-card-body title="Horizontal Card">
+                        <b-card-text>
+                          {{ text }}
+                        </b-card-text>
+                      </b-card-body>
+                    </b-col>
+                  </b-row>
+                </b-card>
+              </b-col>
+            </b-row>
+            <br>
+            <h4 id="list-item-2">Casual Tops</h4>
+            <b-row style="width: 1650px;">
+              <b-col v-for="n in 3" :key="n">
+                <b-card no-body style="width: 540px;">
+                  <b-row no-gutters>
+                    <b-col md="6">
+                      <b-card-img src="../assets/corp_photos/corp1.jpg"
+                      class="rounded-0"></b-card-img>
+                    </b-col>
+                    <b-col md="6">
+                      <b-card-body title="Horizontal Card">
+                        <b-card-text>
+                          {{ text }}
+                        </b-card-text>
+                      </b-card-body>
+                    </b-col>
+                  </b-row>
+                </b-card>
+              </b-col>
+            </b-row>
+            <br>
+            <h4 id="list-item-3">Corporate</h4>
+            <b-row style="width: 1650px;">
+              <b-col v-for="n in 3" :key="n">
+                <b-card no-body style="width: 540px;">
+                  <b-row no-gutters>
+                    <b-col md="6">
+                      <b-card-img src="../assets/formal_photos/form1.jpg"
+                      class="rounded-0"></b-card-img>
+                    </b-col>
+                    <b-col md="6">
+                      <b-card-body title="Horizontal Card">
+                        <b-card-text>
+                          {{ text }}
+                        </b-card-text>
+                      </b-card-body>
+                    </b-col>
+                  </b-row>
+                </b-card>
+              </b-col>
+            </b-row>
+            <br>
+            <h4 id="list-item-4">Formal Dresses</h4>
+            <b-row style="width: 1650px;">
+              <b-col v-for="n in 3" :key="n">
+                <b-card no-body style="width: 540px;">
+                  <b-row no-gutters>
+                    <b-col md="6">
+                      <b-card-img src="../assets/formal_photos/formcoat1.jpg"
+                      class="rounded-0"></b-card-img>
+                    </b-col>
+                    <b-col md="6">
+                      <b-card-body title="Horizontal Card">
+                        <b-card-text>
+                          {{ text }}
+                        </b-card-text>
+                      </b-card-body>
+                    </b-col>
+                  </b-row>
+                </b-card>
+              </b-col>
+            </b-row>
+            <br>
+            <h4 id="list-item-5">Formal Coat</h4>
+            <b-row style="width: 1650px;">
+              <b-col v-for="n in 3" :key="n">
+                <b-card no-body style="width: 540px;">
+                  <b-row no-gutters>
+                    <b-col md="6">
+                      <b-card-img src="../assets/casual_photos/casual2.jpg"
+                      class="rounded-0"></b-card-img>
+                    </b-col>
+                    <b-col md="6">
+                      <b-card-body title="Horizontal Card">
+                        <b-card-text>
+                          {{ text }}
+                        </b-card-text>
+                      </b-card-body>
+                    </b-col>
+                  </b-row>
+                </b-card>
+              </b-col>
+            </b-row>
+          </div>
+        </b-col>
+      </b-card-body>
+    </b-card>
+  </div>
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        text: `
+export default {
+  methods: {
+    // Convenience method to scroll a heading into view.
+    // Not required for scrollspy to work
+    scrollIntoView(evt) {
+      evt.preventDefault();
+      const href = evt.target.getAttribute('href');
+      const el = href ? document.querySelector(href) : null;
+      if (el) {
+        this.$refs.content.scrollTop = el.offsetTop;
+      }
+    },
+  },
+  data() {
+    return {
+      text: `
           Quis magna Lorem anim amet ipsum do mollit sit cillum voluptate ex nulla
           tempor. Laborum consequat non elit enim exercitation cillum aliqua
           consequat id aliqua. Esse ex consectetur mollit voluptate est in duis
           laboris ad sit ipsum anim Lorem. Incididunt veniam velit elit elit veniam
-        `
-      }
-    }
-  }
+          Lorem aliqua quis ullamco deserunt sit enim elit aliqua esse irure. Laborum
+          nisi sit est tempor laborum mollit labore officia laborum excepteur
+          commodo non commodo dolor excepteur commodo. Ipsum fugiat ex est consectetur
+          ipsum commodo tempor sunt in proident.
+        `,
+    };
+  },
+};
 </script>
-
 <style>
-  .example{
+
+  /* Hide scrollbar for Chrome, Safari and Opera */
+
+  .nav  {
+  text-decoration: none;  
+  font-family: "Lucida Sans", "Lucida Sans Regular", "Lucida Grande",
+  "Lucida Sans Unicode", Geneva, Verdana, sans-serif;
+
+  }
+
+  .nav li a {
+     color: white;
+     background-color: #e9ecea;
+  }
+  .nav li :hover{
+     color: #b09545;
+  }
+
+.nav-pills .nav-link.active, .nav-pills .show > .nav-link {
+    color: #b09545;
+    background-color: #e9ecea;
+}
+
+  .nav-scroller{
+    color: #777
+  }
+ 
+  .card-header{
+    position: sticky;
+    top: 0%;
+    z-index: 1;
+
+    background-color: #e9ecea;
+  }
+
+  .my-content{
     position:relative;
     overflow: scroll; /* Add the ability to scroll */
     height:400px;
   }
-  /* Hide scrollbar for Chrome, Safari and Opera */
-  .example::-webkit-scrollbar {
+
+  .my-content::-webkit-scrollbar {
     display: none;
   }
-  #my-list-col{
-    position: relative;
-    overflow: hidden;
-  }
-  #my-list{
-    /* position: absolute;
-    top: 20%;
-    left:5%;
-    width: 90%; */
-  }
+  
 </style>

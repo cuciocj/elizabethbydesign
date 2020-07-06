cb02884ce7f0d6fada8cd0887b2c89c359c9ecf9
psantander <30874349+psantander@users.noreply.github.com>
2020-07-06 12:31:23 +1200

    Update Where
    Add photos and texts

--- src/views/Where.vue
+++ src/views/Where.vue
@@ -15,7 +15,7 @@
           <div id="listgroup-ex">
             <h4 id="list-item-1">Casual Dresses</h4>
             <b-row style="width: 1650px;">
-              <b-col v-for="n in 3" :key="n">
+              <b-col>
                 <b-card no-body style="width: 540px;">
                   <b-row no-gutters>
                     <b-col md="6">
@@ -23,9 +23,43 @@
                       class="rounded-0"></b-card-img>
                     </b-col>
                     <b-col md="6">
-                      <b-card-body title="Horizontal Card">
+                      <b-card-body title="Silk Dress">
                         <b-card-text>
-                          {{ text }}
+                          {{ text1 }}
+                        </b-card-text>
+                      </b-card-body>
+                    </b-col>
+                  </b-row>
+                </b-card>
+              </b-col>
+              <b-col>
+                <b-card no-body style="width: 540px;">
+                  <b-row no-gutters>
+                    <b-col md="6">
+                      <b-card-img src="../assets/casual_photos/casual4.jpg"
+                      class="rounded-0"></b-card-img>
+                    </b-col>
+                    <b-col md="6">
+                      <b-card-body title="Handprinting Dress">
+                        <b-card-text>
+                          {{ text2 }}
+                        </b-card-text>
+                      </b-card-body>
+                    </b-col>
+                  </b-row>
+                </b-card>
+              </b-col>
+              <b-col>
+                <b-card no-body style="width: 540px;">
+                  <b-row no-gutters>
+                    <b-col md="6">
+                      <b-card-img src="../assets/casual_photos/casual6.jpg"
+                      class="rounded-0"></b-card-img>
+                    </b-col>
+                    <b-col md="6">
+                      <b-card-body title="Floral Summer Dress">
+                        <b-card-text>
+                          {{ text3 }}
                         </b-card-text>
                       </b-card-body>
                     </b-col>
@@ -36,17 +70,51 @@
             <br>
             <h4 id="list-item-2">Casual Tops</h4>
             <b-row style="width: 1650px;">
-              <b-col v-for="n in 3" :key="n">
+              <b-col >
                 <b-card no-body style="width: 540px;">
                   <b-row no-gutters>
                     <b-col md="6">
-                      <b-card-img src="../assets/corp_photos/corp1.jpg"
+                      <b-card-img src="../assets/casual_photos/casual7.jpg"
                       class="rounded-0"></b-card-img>
                     </b-col>
                     <b-col md="6">
-                      <b-card-body title="Horizontal Card">
+                      <b-card-body title="Italian Silk Top">
                         <b-card-text>
-                          {{ text }}
+                          {{ text4 }}
+                        </b-card-text>
+                      </b-card-body>
+                    </b-col>
+                  </b-row>
+                </b-card>
+              </b-col>
+              <b-col >
+                <b-card no-body style="width: 540px;">
+                  <b-row no-gutters>
+                    <b-col md="6">
+                      <b-card-img src="../assets/casual_photos/casual8.jpg"
+                      class="rounded-0"></b-card-img>
+                    </b-col>
+                    <b-col md="6">
+                      <b-card-body title="Floral Blue Top">
+                        <b-card-text>
+                          {{ text5 }}
+                        </b-card-text>
+                      </b-card-body>
+                    </b-col>
+                  </b-row>
+                </b-card>
+              </b-col>
+              <b-col >
+                <b-card no-body style="width: 540px;">
+                  <b-row no-gutters>
+                    <b-col md="6">
+                      <b-card-img src="../assets/casual_photos/casual10.jpg"
+                      class="rounded-0"></b-card-img>
+                    </b-col>
+                    <b-col md="6">
+                      <b-card-body title="Floral Gold Top">
+                        <b-card-text>
+                          {{ text6 }}
                         </b-card-text>
                       </b-card-body>
                     </b-col>
@@ -57,28 +125,81 @@
             <br>
             <h4 id="list-item-3">Corporate</h4>
             <b-row style="width: 1650px;">
-              <b-col v-for="n in 3" :key="n">
+              <b-col>
                 <b-card no-body style="width: 540px;">
                   <b-row no-gutters>
                     <b-col md="6">
-                      <b-card-img src="../assets/formal_photos/form1.jpg"
+                      <b-card-img src="../assets/corp_photos/corp3.jpg"
                       class="rounded-0"></b-card-img>
                     </b-col>
                     <b-col md="6">
-                      <b-card-body title="Horizontal Card">
+                      <b-card-body title="Floral Coorporate Dress">
                         <b-card-text>
-                          {{ text }}
+                          {{ text7 }}
                         </b-card-text>
                       </b-card-body>
                     </b-col>
                   </b-row>
                 </b-card>
               </b-col>
+              <b-col>
+                <b-card no-body style="width: 540px;">
+                  <b-row no-gutters>
+                    <b-col md="6">
+                      <b-card-img src="../assets/corp_photos/corp4.jpg"
+                      class="rounded-0"></b-card-img>
+                    </b-col>
+                    <b-col md="6">
+                      <b-card-body title="Black Coorporate Dress">
+                        <b-card-text>
+                          {{ text8 }}
+                        </b-card-text>
+                      </b-card-body>
+                    </b-col>
+                  </b-row>
+                </b-card>
+              </b-col>
+
             </b-row>
             <br>
-            <h4 id="list-item-4">Formal Dresses</h4>
+            <h4 id="list-item-4">Formal</h4>
             <b-row style="width: 1650px;">
-              <b-col v-for="n in 3" :key="n">
+              <b-col>
+                <b-card no-body style="width: 540px;">
+                  <b-row no-gutters>
+                    <b-col md="6">
+                      <b-card-img src="../assets/formal_photos/form2.jpg"
+                      class="rounded-0"></b-card-img>
+                    </b-col>
+                    <b-col md="6">
+                      <b-card-body title="Black Formal Dress">
+                        <b-card-text>
+                          {{ text9 }}
+                        </b-card-text>
+                      </b-card-body>
+                    </b-col>
+                  </b-row>
+                </b-card>
+              </b-col>
+                            <b-col>
+                <b-card no-body style="width: 540px;">
+                  <b-row no-gutters>
+                    <b-col md="6">
+                      <b-card-img src="../assets/formal_photos/form4.jpg"
+                      class="rounded-0"></b-card-img>
+                    </b-col>
+                    <b-col md="6">
+                      <b-card-body title="Floral Formal Dress">
+                        <b-card-text>
+                          {{ text10 }}
+                        </b-card-text>
+                      </b-card-body>
+                    </b-col>
+                  </b-row>
+                </b-card>
+              </b-col>
+
+              <b-col>
                 <b-card no-body style="width: 540px;">
                   <b-row no-gutters>
                     <b-col md="6">
@@ -86,37 +207,18 @@
                       class="rounded-0"></b-card-img>
                     </b-col>
                     <b-col md="6">
-                      <b-card-body title="Horizontal Card">
+                      <b-card-body title="Evening Coat">
                         <b-card-text>
-                          {{ text }}
+                          {{ text11 }}
                         </b-card-text>
                       </b-card-body>
                     </b-col>
                   </b-row>
                 </b-card>
               </b-col>
+
             </b-row>
             <br>
-            <h4 id="list-item-5">Formal Coat</h4>
-            <b-row style="width: 1650px;">
-              <b-col v-for="n in 3" :key="n">
-                <b-card no-body style="width: 540px;">
-                  <b-row no-gutters>
-                    <b-col md="6">
-                      <b-card-img src="../assets/casual_photos/casual2.jpg"
-                      class="rounded-0"></b-card-img>
-                    </b-col>
-                    <b-col md="6">
-                      <b-card-body title="Horizontal Card">
-                        <b-card-text>
-                          {{ text }}
-                        </b-card-text>
-                      </b-card-body>
-                    </b-col>
-                  </b-row>
-                </b-card>
-              </b-col>
-            </b-row>
           </div>
         </b-col>
       </b-card-body>
@@ -140,16 +242,64 @@ export default {
   },
   data() {
     return {
-      text: `
-          Quis magna Lorem anim amet ipsum do mollit sit cillum voluptate ex nulla
-          tempor. Laborum consequat non elit enim exercitation cillum aliqua
-          consequat id aliqua. Esse ex consectetur mollit voluptate est in duis
-          laboris ad sit ipsum anim Lorem. Incididunt veniam velit elit elit veniam
-          Lorem aliqua quis ullamco deserunt sit enim elit aliqua esse irure. Laborum
-          nisi sit est tempor laborum mollit labore officia laborum excepteur
-          commodo non commodo dolor excepteur commodo. Ipsum fugiat ex est consectetur
-          ipsum commodo tempor sunt in proident.
+      text1: `
+          This is a lovely silk dress that feels just great to wear. Team it up with a higher heel 
+          or a pair of white sneakers.
+          This dress is fully lined.
         `,
+       text2: `
+          This dress is special.
+          The fabric was sourced from Gardams of Australia and is handprinted.
+          I always feel special wearing this out to lunch dates. Dress it up with heels or pop a pair 
+          of flats on to make it more casual. 
+          This dress is fully lined.
+        `,       
+        text3:` 
+        This dress is special. The fabric was sourced from Gardams of Australia and is handprinted.
+        I always feel special wearing this out to lunch dates. Dress it up with heels or pop a pair 
+        of flats on to make it more casual. 
+        This dress is fully lined.
+        `,
+        text4:`
+        Love this Italian silk top. It has pleats on the neck line providing a lovely drape. 
+        This top feels fabulous to wear. Team it up with jeans for a dressed up casual look.
+        `,
+        text5:`
+        This is such a fun top. It has a yoke front and back providing a lovely drape. Worn a 
+        little shorter team it up with jeans or a skirt..
+        `,
+        text6:`
+        A real favourite of mine. Tailored fit with hemline slit,. Really flattering with an empire line.
+        This top could be worn with a more formal skirt for work or teamed up with jeans.
+        `,
+        text7:`
+        This dress will take you into any meeting feeling confident and well styled. 
+        The lower cut back line adds a point of interest  and provides a flattering finish.
+        The dress is fully lined through the bodice.
+        `,
+        text8:`
+        A personal favourite of mine. I always feel confident when wearing this dress. 
+        The scalloped hem and sleeve add interest as does the self embossed flowers which add a subtle texture to the dress,
+        This dress is fully lined.
+        `,
+        text9:`
+        This dress is ideal for going out to dinner or to a party. It’s simple lines flatter 
+        the figure and the sparkle on the sleeve adds a bit of interest to the garment.
+        Such a comfortable style to wear.
+        The dress is fully lined through the bodice and has a lovely back kick pleat 
+        for added style.
+        `,
+        text10:`
+        This dress is made from brocade and is ideal for weddings, a day at the races or 
+        any formal occasion where elegance is required.
+        `,
+        text11:`
+        Team this up with any black dress, corporate or formal and you will get no end of compliments. 
+
+       This coat  is fully lined.
+       `
+
+
     };
   },
 };

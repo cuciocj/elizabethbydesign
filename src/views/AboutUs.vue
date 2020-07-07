3edda2b75a8a6cc99e213dab449fbd398cc4c57f
psantander <30874349+psantander@users.noreply.github.com>
2020-07-07 14:58:01 +1200

    Aboutus Update
    Changed the about us page and add photos.

--- src/views/AboutUs.vue
+++ src/views/AboutUs.vue
@@ -1,44 +1,61 @@
 <template>
   <div class="about-content">
     <br><br>
+      <v-row no-gutters>
+        <v-col md="6">
+          <v-img
+            :src="require('@/assets/kerry.png')"
+            height="100vh"
+          />
+        </v-col>
+        <v-col md="">
+        </v-col>
 
+        <v-col
+          class="align-content-space-between layout wrap"
+          cols="12"
+          md="6"
+          :pa-5="$vuetify.breakpoint.smAndDown">
 
-        <v-row >
-          <v-col class="ml-auto" cols="3">
-            <v-avatar color="indigo">
-              <v-icon dark>mdi-account-circle</v-icon>
-            </v-avatar>
-              <v-avatar color="indigo">
-              <v-icon dark>mdi-account-circle</v-icon>
-            </v-avatar>
-            <v-avatar size="200" right>
-              <img
-                src="../assets/kerry.png"
-                alt="John"
-              >
-            </v-avatar>
-          </v-col>
-          <v-col class="justify-space-around" cols="6">
-            <div class="card-1">
-              <div class="card-body">
-                <h5 class="card-title-1">Hello! My name is Kerry Elizabeth Mitchell (Nee Brennan). </h5>
-                <br>
-                <p class="card-text"> I am 62 years young and  I want to share with you my story and why 
-                  I have developed such a drive and passion for designing and creating clothing for the mature woman.
-                  <br><br>
-                  At the age of just 12 years of age my grandmother Elizabeth Brennan, sat me down in front of an
-                   old Singer treadmill sewing machine and helped me to make my very first garment. 
-                   It was my high school uniform, complete with boxed pleats and insert pockets. 
-                   My Grandma guided and encouraged me every step of the way.  
-                   I thought I was the cleverest person alive.<br><br>
+            <v-col class="justify-space-around" cols="12">
+              <div class="card-1" height="100vh">
+                <div class="card-body">
+                  <h5 class="card-title-1">Hello! My name is Kerry Elizabeth Mitchell (Nee Brennan). </h5>
+                  <br>
+                  <p class="card-text"> I am 62 years young and  I want to share with you my story and why 
+                    I have developed such a drive and passion for designing and creating clothing for the mature woman.
+                    <br><br>
+                    At the age of just 12 years of age my grandmother Elizabeth Brennan, sat me down in front of an
+                    old Singer treadmill sewing machine and helped me to make my very first garment. 
+                    It was my high school uniform, complete with boxed pleats and insert pockets. 
+                    My Grandma guided and encouraged me every step of the way.  
+                    I thought I was the cleverest person alive.<br><br>
 
-                   I remember the feeling of amazement knowing that I had created this garment from
+                    I remember the feeling of amazement knowing that I had created this garment from
                     what had started out as a flat piece of woven cloth. My love of sewing grew from 
                     there. Over the years I have made many garments for myself, family and friends. 
                     Designing and  sewing was not to be a career pathway but rather a much loved 
                     hobby that I was very good at.<br><br>
+                  </p>
+                </div>
+              </div>
+            </v-col>
+        </v-col>
+      </v-row>
 
-                    My career took me to the education sector and then to the corporate world where I
+      <v-row no-gutters>
+        <v-col md="1">
+        </v-col>
+        <v-col
+          class="align-content-space-between layout wrap" cols="7" md="4"
+          :pa-5="$vuetify.breakpoint.smAndDown">
+
+            <v-col class="justify-space-around" cols="12">
+              <div class="card-1" height="100vh">
+                <div class="card-body">
+                  <h5 class="card-title-1"> </h5>
+                  <br>
+                  <p class="card-text"> My career took me to the education sector and then to the corporate world where I
                      facilitated meetings, spoke at conferences, and worked with people every day. 
                      I always needed to look my best.<br><br>
 
@@ -51,10 +68,44 @@
                     My friends tell me that as they have aged, it has become harder to find 
                     styles that work for them. A walk through any shopping area will show 
                     that many shops cater for the young woman and at best make a poor attempt
-                     to cater for the mature woman. Clothing is either completely the wrong style
+                     to cater for the mature woman.
+                  </p>
+                </div>
+              </div>
+            </v-col>
+          </v-col>
+          <v-col md="">
+          </v-col>
+            <v-col md="6">
+              <v-img
+                :src="require('@/assets/aboutme.png')"
+                height="100vh"/>
+            </v-col>
+      </v-row>
+
+      <v-row no-gutters>
+        <v-col md="6">
+          <v-img
+            :src="require('@/assets/services.png')"
+            height="100vh"
+          />
+          </v-col>
+          <v-col md="">
+          </v-col>
+
+        <v-col
+          class="align-content-space-between layout wrap" cols="6" md="4"
+          :pa-5="$vuetify.breakpoint.smAndDown">
+
+            <v-col class="justify-space-around" cols="12">
+              <div class="card-1" height="100vh">
+                <div class="card-body">
+                  <p class="card-text" justify>
+        
+                      Clothing is either completely the wrong style
                       and shape for the mature woman or what is available is dull, baggy and not made 
                       to show off our best features.<br><br>
-
+                      
                       The lack of beautifully designed clothing for the mature woman has led me back 
                       to my love of sewing and design but with the more mature woman in mind. 
                       I get great joy in working with a woman to work out what garments she 
@@ -64,83 +115,81 @@
                       The mature woman deserves to look fabulous and together we can achieve your
                        own style of fabulous.  I would be delighted to offer my services to work 
                        with you to design and create clothing that can take the mature woman 
-                       anywhere she wants to go!
-                </p>
+                       anywhere she wants to go!way but rather a much loved 
+                    hobby that I was very good at.
+                  </p>
+                </div>
               </div>
-            </div>
-          </v-col>
-          <v-col cols="2">
-          </v-col>
-        </v-row>
+            </v-col>
+        </v-col>
+        <v-col md="">
+        </v-col>
+      </v-row>
 
-    
+      <b-card
+        overlay
+        img-src="https://picsum.photos/900/250/?image=3"
+        img-alt="Card Image"
+        text-variant="white"
+        title="How Does It  Work?">
+          <b-card-text>
+                <br><br>At Elizabeth by Design we design and make clothing for the mature woman. 
+                We are based in Auckland and we deliver anywhere in New Zealand.<br><br>
+                Typically it takes 6 weeks to make a garment. This includes consultation
+                about design, making a mockup to make sure we have the design right, 
+                purchase <br>of fabric and then creation and fitting process.<br><br>
+                We use both face to face and online platforms to meet and discuss what 
+                garment you are hoping to have made. 
+        </b-card-text>
+      </b-card>
 
-        <v-timeline class="project-about-timeline">
-          <v-timeline-item color="#c8a338" large>
-          <template v-slot:opposite>
-            <span></span>
-          </template>
-          <v-card class="elevation-2">
-            <v-card-title class="headline">How Does It  Work?</v-card-title>
-            <v-card-text>
-              At Elizabeth by Design we design and make clothing for the mature woman. 
-              We are based in Auckland and we deliver anywhere in New Zealand.<br><br>
-              Typically it takes 6 weeks to make a garment. This includes consultation
-               about design, making a mockup to make sure we have the design right, 
-               purchase <br>of fabric and then creation and fitting process.<br><br>
-              We use both face to face and online platforms to meet and discuss what 
-              garment you are hoping to have made. 
-            </v-card-text>
-          </v-card>
-        </v-timeline-item>
-
-         <v-timeline-item color="#c8a338" large>
+    <v-timeline class="project-about-timeline">
+       <v-timeline-item color="#c8a338" large>
           <template v-slot:opposite>
           </template>
           <v-card class="elevation-2">
-            <v-card-title class="headline" >Step One: Getting Started</v-card-title>
+          <v-card-title class="headline" >Step One: Getting Started</v-card-title>
             <v-card-text>
               You might like to start by  going to our <b>Understanding of Your Style page</b>.
               This page asks some questions and provides guidance about knowing what works for you.<br><br>
               Also Have a look at our gallery of photos on our <b>Where are you going?</b> page. 
               We do not sell off the shelf and so images you view are one-off garments.
-               These photos might give you some ideas about what you are looking for. 
+              These photos might give you some ideas about what you are looking for. 
               Kerry will work directly with you to design exactly what you want.<br><br>
               Once you are ready, use our <b>Get In Touch</b> page to make contact with Kerry. You can call Kerry or email her with a preferred time to meet. Meetings can be online or face to face - depending on where you live.
             </v-card-text>
           </v-card>
-        </v-timeline-item>
+       </v-timeline-item>
 
-          <v-timeline-item color="#c8a338" large>
+        <v-timeline-item color="#c8a338" large>
           <template v-slot:opposite>
             <span></span>
           </template>
           <v-card class="elevation-2">
             <v-card-title class="headline">Step Two: Design Phase</v-card-title>
-            <v-card-text>
-              Meet with Kerry either face to face or online. 
-              Allow about an hour to talk through the style colours, fabrics for your 
-              garment. At this time we will take your body measurements. <br><br>
-              Kerry will provide you with drawings of the garment for your approval. 
-              Once approved Kerry will make up a mockup of the garment, if you choose
-               this option (recommended for formal dresses, coats and casual dresses).
-            </v-card-text>
+              <v-card-text>
+                Meet with Kerry either face to face or online. 
+                Allow about an hour to talk through the style colours, fabrics for your 
+                garment. At this time we will take your body measurements. <br><br>
+                Kerry will provide you with drawings of the garment for your approval. 
+                Once approved Kerry will make up a mockup of the garment, if you choose
+                this option (recommended for formal dresses, coats and casual dresses).
+              </v-card-text>
           </v-card>
         </v-timeline-item>
 
-          <v-timeline-item color="#c8a338" large>
+        <v-timeline-item color="#c8a338" large>
           <template v-slot:opposite>
             <span></span>
           </template>
           <v-card class="elevation-2">
             <v-card-title class="headline">Step Three: Creation of the Garment </v-card-title>
-            <v-card-text>
-              Once we are confident we have the right style and we know the fabric we will use, 
-              Kerry will create your garment. A final fitting is recommended before completion. 
-            </v-card-text>
-          </v-card>
+              <v-card-text>
+                Once we are confident we have the right style and we know the fabric we will use, 
+                Kerry will create your garment. A final fitting is recommended before completion. 
+              </v-card-text>
+            </v-card>
         </v-timeline-item>
-       
       </v-timeline>
 
   </div>

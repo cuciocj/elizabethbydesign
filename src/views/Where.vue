4c328e55143198831455f72e90632cb8545142d7
Juan Fernando Parra <jparrax17@gmail.com>
2020-07-13 13:50:07 +1200

    Retrieve from db where content

--- src/views/Where.vue
+++ src/views/Where.vue
@@ -3,227 +3,34 @@
     <b-card no-body>
       <b-tabs pills card fill>
         <b-card-body id="nav-scroller" class="my-content" ref="content" style="position:relative; height:430px;">
-          <b-tab title="Casual Dresses" active v-on:click="scrollToBeginning">
-          
-            <b-row style="width: 1650px;">
+
+          <b-tab v-for="tab in object.data" :key="tab" :title="tab.title" v-on:click="scrollToBeginning">
+            <b-row :style="{width: tab.width}">
               <b-icon-caret-left data-direction="left" class="arrow-move-left"  font-scale="2" v-if="arrow_initial" v-on:mouseover="startArrowAnimation" ></b-icon-caret-left>
               <b-icon-caret-left-fill class="arrow-move-left" font-scale="2" v-if="arrow_hover" animation="cylon" v-on:mouseout="stopArrowAnimation"></b-icon-caret-left-fill>
-              <b-col>
+
+              <b-col v-for="card in tab.cards" :key="card">
                 <b-card border-variant="light" no-body style="width: 540px;">
                   <b-row no-gutters>
                     <b-col md="6">
-                      <b-card-img src="../assets/casual_photos/casual2.jpg"
-                      class="rounded-0"></b-card-img>
+                      <b-card-img :src="card.img1" class="rounded-0"></b-card-img>
                     </b-col>
                     <b-col md="6">
-                      <b-card-body title="Silk Dress">
-                        <b-card-text>
-                          {{ text1 }}
+                      <b-card-body :title="card.title">
+                        <b-card-text style="text-align:justify;">
+                          {{ card.content }}
                         </b-card-text>
                       </b-card-body>
                     </b-col>
                   </b-row>
                 </b-card>
               </b-col>
-              <b-col>
-                <b-card no-body border-variant="light" style="width: 540px;">
-                  <b-row no-gutters>
-                    <b-col md="6">
-                      <b-card-img src="../assets/casual_photos/casual4.jpg"
-                      class="rounded-0"></b-card-img>
-                    </b-col>
-                    <b-col md="6">
-                      <b-card-body title="Handprinting Dress">
-                        <b-card-text>
-                          {{ text2 }}
-                        </b-card-text>
-                      </b-card-body>
-                    </b-col>
-                  </b-row>
-                </b-card>
-              </b-col>
-              <b-col>
-                <b-card no-body border-variant="light" style="width: 540px;">
-                  <b-row no-gutters>
-                    <b-col md="6">
-                      <b-card-img src="../assets/casual_photos/casual6.jpg"
-                      class="rounded-0"></b-card-img>
-                    </b-col>
-                    <b-col md="6">
-                      <b-card-body title="Floral Summer Dress">
-                        <b-card-text>
-                          {{ text3 }}
-                        </b-card-text>
-                      </b-card-body>
-                    </b-col>
-                  </b-row>
-                </b-card>
-              </b-col>
+
               <b-icon-caret-right data-direction="right" class="arrow-move-right"  font-scale="2" v-if="arrow_initial" v-on:mouseover="startArrowAnimation" ></b-icon-caret-right>
               <b-icon-caret-right-fill class="arrow-move-right" font-scale="2" v-if="arrow_hover" animation="cylon" v-on:mouseout="stopArrowAnimation"></b-icon-caret-right-fill>
             </b-row>
+          </b-tab>
           
-          </b-tab>
-          <b-tab title="Casual Tops" v-on:click="scrollToBeginning">
-            <b-row style="width: 1650px;">
-              <b-icon-caret-left data-direction="left" class="arrow-move-left"  font-scale="2" v-if="arrow_initial" v-on:mouseover="startArrowAnimation" ></b-icon-caret-left>
-              <b-icon-caret-left-fill class="arrow-move-left" font-scale="2" v-if="arrow_hover" animation="cylon" v-on:mouseout="stopArrowAnimation"></b-icon-caret-left-fill>
-              <b-col >
-                <b-card no-body border-variant="light" style="width: 540px;">
-                  <b-row no-gutters>
-                    <b-col md="6">
-                      <b-card-img src="../assets/casual_photos/casual7.jpg"
-                      class="rounded-0"></b-card-img>
-                    </b-col>
-                    <b-col md="6">
-                      <b-card-body title="Italian Silk Top">
-                        <b-card-text>
-                          {{ text4 }}
-                        </b-card-text>
-                      </b-card-body>
-                    </b-col>
-                  </b-row>
-                </b-card>
-              </b-col>
-              <b-col >
-                <b-card no-body border-variant="light" style="width: 540px;">
-                  <b-row no-gutters>
-                    <b-col md="6">
-                      <b-card-img src="../assets/casual_photos/casual8.jpg"
-                      class="rounded-0"></b-card-img>
-                    </b-col>
-                    <b-col md="6">
-                      <b-card-body title="Floral Blue Top">
-                        <b-card-text>
-                          {{ text5 }}
-                        </b-card-text>
-                      </b-card-body>
-                    </b-col>
-                  </b-row>
-                </b-card>
-              </b-col>
-              <b-col >
-                <b-card no-body border-variant="light" style="width: 540px;">
-                  <b-row no-gutters>
-                    <b-col md="6">
-                      <b-card-img src="../assets/casual_photos/casual10.jpg"
-                      class="rounded-0"></b-card-img>
-                    </b-col>
-                    <b-col md="6">
-                      <b-card-body title="Floral Gold Top">
-                        <b-card-text>
-                          {{ text6 }}
-                        </b-card-text>
-                      </b-card-body>
-                    </b-col>
-                  </b-row>
-                </b-card>
-              </b-col>
-              <b-icon-caret-right data-direction="right" class="arrow-move-right"  font-scale="2" v-if="arrow_initial" v-on:mouseover="startArrowAnimation" ></b-icon-caret-right>
-              <b-icon-caret-right-fill class="arrow-move-right" font-scale="2" v-if="arrow_hover" animation="cylon" v-on:mouseout="stopArrowAnimation"></b-icon-caret-right-fill>
-            </b-row>
-          </b-tab>
-          <b-tab title="Coorporate dresses" v-on:click="scrollToBeginning">
-            <b-row style="width: 1300px;">
-              <b-icon-caret-left data-direction="left" class="arrow-move-left"  font-scale="2" v-if="arrow_initial" v-on:mouseover="startArrowAnimation" ></b-icon-caret-left>
-              <b-icon-caret-left-fill class="arrow-move-left" font-scale="2" v-if="arrow_hover" animation="cylon" v-on:mouseout="stopArrowAnimation"></b-icon-caret-left-fill>
-              <b-col>
-                <b-card no-body border-variant="light" style="width: 540px;">
-                  <b-row no-gutters>
-                    <b-col md="6">
-                      <b-card-img src="../assets/corp_photos/corp3.jpg"
-                      class="rounded-0"></b-card-img>
-                    </b-col>
-                    <b-col md="6">
-                      <b-card-body title="Floral Coorporate Dress">
-                        <b-card-text>
-                          {{ text7 }}
-                        </b-card-text>
-                      </b-card-body>
-                    </b-col>
-                  </b-row>
-                </b-card>
-              </b-col>
-              <b-col>
-                <b-card no-body border-variant="light" style="width: 540px;">
-                  <b-row no-gutters>
-                    <b-col md="6">
-                      <b-card-img src="../assets/corp_photos/corp4.jpg"
-                      class="rounded-0"></b-card-img>
-                    </b-col>
-                    <b-col md="6">
-                      <b-card-body title="Black Coorporate Dress">
-                        <b-card-text>
-                          {{ text8 }}
-                        </b-card-text>
-                      </b-card-body>
-                    </b-col>
-                  </b-row>
-                </b-card>
-              </b-col>
-              <b-icon-caret-right data-direction="right" class="arrow-move-right"  font-scale="2" v-if="arrow_initial" v-on:mouseover="startArrowAnimation" ></b-icon-caret-right>
-              <b-icon-caret-right-fill class="arrow-move-right" font-scale="2" v-if="arrow_hover" animation="cylon" v-on:mouseout="stopArrowAnimation"></b-icon-caret-right-fill>
-            </b-row>
-          </b-tab>
-          <b-tab title="Formal Dresses" v-on:click="scrollToBeginning">
-            <b-row style="width: 1650px;">
-              <b-icon-caret-left data-direction="left" class="arrow-move-left"  font-scale="2" v-if="arrow_initial" v-on:mouseover="startArrowAnimation" ></b-icon-caret-left>
-              <b-icon-caret-left-fill class="arrow-move-left" font-scale="2" v-if="arrow_hover" animation="cylon" v-on:mouseout="stopArrowAnimation"></b-icon-caret-left-fill>
-              <b-col>
-                <b-card no-body border-variant="light" style="width: 540px;">
-                  <b-row no-gutters>
-                    <b-col md="6">
-                      <b-card-img src="../assets/formal_photos/form2.jpg"
-                      class="rounded-0"></b-card-img>
-                    </b-col>
-                    <b-col md="6">
-                      <b-card-body title="Black Formal Dress">
-                        <b-card-text>
-                          {{ text9 }}
-                        </b-card-text>
-                      </b-card-body>
-                    </b-col>
-                  </b-row>
-                </b-card>
-              </b-col>
-              <b-col>
-                <b-card no-body border-variant="light" style="width: 540px;">
-                  <b-row no-gutters>
-                    <b-col md="6">
-                      <b-card-img src="../assets/formal_photos/form4.jpg"
-                      class="rounded-0"></b-card-img>
-                    </b-col>
-                    <b-col md="6">
-                      <b-card-body title="Floral Formal Dress">
-                        <b-card-text>
-                          {{ text10 }}
-                        </b-card-text>
-                      </b-card-body>
-                    </b-col>
-                  </b-row>
-                </b-card>
-              </b-col>
-              <b-col>
-                <b-card no-body border-variant="light" style="width: 540px;">
-                  <b-row no-gutters>
-                    <b-col md="6">
-                      <b-card-img src="../assets/formal_photos/formcoat1.jpg"
-                      class="rounded-0"></b-card-img>
-                    </b-col>
-                    <b-col md="6">
-                      <b-card-body title="Evening Coat">
-                        <b-card-text>
-                          {{ text11 }}
-                        </b-card-text>
-                      </b-card-body>
-                    </b-col>
-                  </b-row>
-                </b-card>
-              </b-col>
-              <b-icon-caret-right data-direction="right" class="arrow-move-right"  font-scale="2" v-if="arrow_initial" v-on:mouseover="startArrowAnimation" ></b-icon-caret-right>
-              <b-icon-caret-right-fill class="arrow-move-right" font-scale="2" v-if="arrow_hover" animation="cylon" v-on:mouseout="stopArrowAnimation"></b-icon-caret-right-fill>
-            </b-row>
-          </b-tab>
         </b-card-body>
       </b-tabs>
     </b-card>
@@ -231,65 +38,12 @@
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   data() {
     return {
-      text1: `
-          This is a lovely silk dress that feels just great to wear. Team it up with a higher heel 
-          or a pair of white sneakers.
-          This dress is fully lined.
-        `,
-       text2: `
-          This dress is special.
-          The fabric was sourced from Gardams of Australia and is handprinted.
-          I always feel special wearing this out to lunch dates. Dress it up with heels or pop a pair 
-          of flats on to make it more casual. 
-          This dress is fully lined.
-        `,       
-        text3:` 
-        This dress is special. The fabric was sourced from Gardams of Australia and is handprinted.
-        I always feel special wearing this out to lunch dates. Dress it up with heels or pop a pair 
-        of flats on to make it more casual. 
-        This dress is fully lined.
-        `,
-        text4:`
-        Love this Italian silk top. It has pleats on the neck line providing a lovely drape. 
-        This top feels fabulous to wear. Team it up with jeans for a dressed up casual look.
-        `,
-        text5:`
-        This is such a fun top. It has a yoke front and back providing a lovely drape. Worn a 
-        little shorter team it up with jeans or a skirt..
-        `,
-        text6:`
-        A real favourite of mine. Tailored fit with hemline slit,. Really flattering with an empire line.
-        This top could be worn with a more formal skirt for work or teamed up with jeans.
-        `,
-        text7:`
-        This dress will take you into any meeting feeling confident and well styled. 
-        The lower cut back line adds a point of interest  and provides a flattering finish.
-        The dress is fully lined through the bodice.
-        `,
-        text8:`
-        A personal favourite of mine. I always feel confident when wearing this dress. 
-        The scalloped hem and sleeve add interest as does the self embossed flowers which add a subtle texture to the dress,
-        This dress is fully lined.
-        `,
-        text9:`
-        This dress is ideal for going out to dinner or to a party. It’s simple lines flatter 
-        the figure and the sparkle on the sleeve adds a bit of interest to the garment.
-        Such a comfortable style to wear.
-        The dress is fully lined through the bodice and has a lovely back kick pleat 
-        for added style.
-        `,
-        text10:`
-        This dress is made from brocade and is ideal for weddings, a day at the races or 
-        any formal occasion where elegance is required.
-        `,
-        text11:`
-        Team this up with any black dress, corporate or formal and you will get no end of compliments. 
-
-       This coat  is fully lined.
-       `,
+      object: "",
        arrow_initial: true,
        arrow_hover: false
     };
@@ -310,7 +64,21 @@ export default {
     },
     scrollToBeginning: function(){
       document.getElementById('nav-scroller').scrollLeft -= 50000;
+    },
+    getWhereInfo() {
+      const endpoint = "http://localhost:5000/where";
+      axios
+        .get(endpoint)
+        .then((res) => {
+          this.object = res;
+        })
+        .catch((error) => {
+          console.error(error);
+        });
     }
+  },
+  created() {
+    this.getWhereInfo();
   }
 };
 </script>
@@ -342,9 +110,6 @@ export default {
   }
  
   .card-header{
-    /* position: sticky;
-    top: 0%;
-    z-index: 1; */
     background-color: #e9ecea;
   }
  
